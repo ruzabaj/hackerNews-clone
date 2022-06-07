10ce424f8291a4e210f6e7f59e182f06fd9c074c
userbaj <64317950+userbaj@users.noreply.github.com>
2022-06-07 16:22:26 +0545

    Login page using glass effect

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import '../scss/login.scss'
 
 const Login = () => {
   return (
-    <div>login</div>
+    <div>
+        <img src='bg-hackerNews.jpg' alt='hackerNews' className='bg-img'/>
+        <div className='containers'>
+            <form>
+                <label to="username">Username:</label>
+                <input type="text"/>
+                <div>
+                    <label to="username">Password:</label>
+                    <input type="password"/> 
+                </div>
+                <button type='submit' className='btn-login'>Login</button>
+                <div>
+                    <a href='forgot'>Forgot password?</a>
+                </div>
+            </form>
+        </div>
+    </div>
   )
 }
 
