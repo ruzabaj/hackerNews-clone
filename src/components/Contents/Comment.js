3e66df1afc7aa6deec7a416302c26a26385107fc
userbaj <64317950+userbaj@users.noreply.github.com>
2022-06-08 18:32:25 +0545

    implemented useEffect

--- src/components/Contents/Comment.js
+++ src/components/Contents/Comment.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import '../../scss/contents.scss';
+import React from "react";
+import "../../scss/contents.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Comment = () => {
+  const [Comment, setComment] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => {
+        console.log(response.data);
+        setComment(response.data[0].name);
+      });
+  }, []);
   return (
     <div className="new-posts">
-    <div className="contents">
-      Comment
-      </div>
-      </div>
-  )
-}
+      {/* {Comment.map((item) => ( */}
+      <div className="contents">{Comment}</div>
+      {/* ))} */}
+    </div>
+  );
+};
 
-export default Comment
+export default Comment;
--- src/components/Contents/New.js
+++ src/components/Contents/New.js
@@ -1,37 +1,70 @@
 import React from "react";
 import '../../scss/contents.scss';
+// import newData from "../../StaticFiles/data";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const New = () => {
+const [data, setdata] = useState("");
+
+useEffect (() => {
+  return () => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then((response)=>{
+      console.log(response.data[0].title);
+      setdata(response.data[1].title)
+    })
+  };
+}, [])
   return (
     <div className="new-posts">
-      <div className="contents">
+      {/* {newData.map((item)=>( */}
+
+        <div className="contents">
         <table>
+          <tbody>
+
           <tr>
-            <p>Hi</p>
+            <p>{data}</p>
           </tr>
           <tr>
             <p>Hello</p>
           </tr>
           <tr>
-            <p>Whatever</p>
+            <div>Whatever</div>
           </tr>
           {/* <tr>
             <p>
-              {" "}
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+            {" "}
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of
+            type and scrambled it to make a type specimen book. It has
+            survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of
+            Lorem Ipsum.
             </p>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
             <p>
+            {" "}
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+              </p>
+              </tr>
+              <tr>
+              <p>
               {" "}
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
@@ -43,10 +76,10 @@ const New = () => {
               containing Lorem Ipsum passages, and more recently with desktop
               publishing software like Aldus PageMaker including versions of
               Lorem Ipsum.
-            </p>
-          </tr>
-          <tr>
-            <p>
+              </p>
+              </tr>
+              <tr>
+              <p>
               {" "}
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
@@ -58,25 +91,12 @@ const New = () => {
               containing Lorem Ipsum passages, and more recently with desktop
               publishing software like Aldus PageMaker including versions of
               Lorem Ipsum.
-            </p>
-          </tr>
-          <tr>
-            <p>
-              {" "}
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
-          </tr> */}
+              </p>
+            </tr> */}
+              </tbody>
         </table>
       </div>
+            {/* ))} */}
     </div>
   );
 };
